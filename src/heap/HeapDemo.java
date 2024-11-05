8a0871f7af4d60d3a27b453a904257221d32c039
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2024-11-05 20:26:25 +0100

    Heap

---
+++ src/heap/HeapDemo.java
@@ -0,0 +1,65 @@
+package heap;
+
+import java.util.Arrays;
+
+public class HeapDemo {
+    public static void main(String[] args) {
+        //int[] A = {5, 13, 2, 25, 7, 17, 20, 8};
+        int[] A = {4, 6, 12, 10, 20, 1, 7};
+        System.out.println("Input: " + Arrays.toString(A));
+        System.out.println("Output:" + Arrays.toString(heapsort(A)));
+    }
+
+    static int[] heapsort(int[] array) {
+        buildHeap(array);
+        int heapSize = array.length - 1;
+
+        for (int i = heapSize; i > 0; i--) {
+            int pom = array[0];
+            array[0] = array[i];
+            array[i] = pom;
+            System.out.println(array[i] + "<->" + pom + " " + Arrays.toString(array));
+
+            heapSize--;
+
+            maxHeapify(array, 0, heapSize);
+        }
+        return array;
+    }
+
+    static int[] buildHeap(int[] array) {
+        int heap = array.length - 1;
+        int startIndex = (heap - 1) / 2;
+
+        for (int i = startIndex; i >= 0; i--) {
+            maxHeapify(array, i, heap);
+        }
+
+        return array;
+    }
+
+    static int[] maxHeapify(int[] array, int i, int heapSize) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest;
+
+        if (left <= heapSize && array[left] > array[i]) {
+            largest = left;
+        } else {
+            largest = i;
+        }
+
+        if (right <= heapSize && array[right] > array[largest]) {
+            largest = right;
+        }
+
+        if (i != largest) {
+            int pom = array[i];
+            array[i] = array[largest];
+            array[largest] = pom;
+            System.out.println(array[i] + "<->" + pom + " " + Arrays.toString(array));
+            maxHeapify(array, largest, heapSize);
+        }
+        return array;
+    }
+}
