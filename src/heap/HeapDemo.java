9832adc57408b13aa31777726f9b746bb6831533
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2024-11-05 21:27:28 +0100

    fix code

--- src/heap/HeapDemo.java
+++ src/heap/HeapDemo.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class HeapDemo {
     public static void main(String[] args) {
         //int[] A = {5, 13, 2, 25, 7, 17, 20, 8};
-        int[] A = {4, 6, 12, 10, 20, 1, 7};
+        int[] A = {5, 13, 2, 25, 7, 17, 20, 8};
         System.out.println("Input: " + Arrays.toString(A));
         System.out.println("Output:" + Arrays.toString(heapsort(A)));
     }
 
     static int[] heapsort(int[] array) {
         buildHeap(array);
-        int heapSize = array.length - 1;
+        int heapSize = array.length;
 
-        for (int i = heapSize; i > 0; i--) {
+        for (int i = heapSize - 1; i > 0; i--) {
             int pom = array[0];
             array[0] = array[i];
             array[i] = pom;
@@ -28,8 +28,8 @@ public class HeapDemo {
     }
 
     static int[] buildHeap(int[] array) {
-        int heap = array.length - 1;
-        int startIndex = (heap - 1) / 2;
+        int heap = array.length;
+        int startIndex = (heap - 2) / 2;
 
         for (int i = startIndex; i >= 0; i--) {
             maxHeapify(array, i, heap);
@@ -43,13 +43,13 @@ public class HeapDemo {
         int right = 2 * i + 2;
         int largest;
 
-        if (left <= heapSize && array[left] > array[i]) {
+        if (left < heapSize && array[left] > array[i]) {
             largest = left;
         } else {
             largest = i;
         }
 
-        if (right <= heapSize && array[right] > array[largest]) {
+        if (right < heapSize && array[right] > array[largest]) {
             largest = right;
         }
 
